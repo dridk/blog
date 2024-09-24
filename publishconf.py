3d2c109ed805a56478624d7e8144f13582c7ef72
sacha <sacha.schutz@pm.me>
2024-09-24 23:22:12 +0200

    update publishconf

--- publishconf.py
+++ publishconf.py
@@ -11,7 +11,7 @@ sys.path.append(os.curdir)
 from pelicanconf import *
 
 SITEURL = 'https://dridk.github.io/blog'
-RELATIVE_URLS = False
+RELATIVE_URLS = True
 
 FEED_ALL_ATOM = 'feeds/all.atom.xml'
 CATEGORY_FEED_ATOM = 'feeds/{slug}.atom.xml'
